0b45a61d225cda85083c00fdda92ba370ef1ebe2
Eva Reitzel <evareitzel@gmail.com>
2022-06-05 22:23:24 -0400

    commit tree a3e539070977783f97b5d774aaa311cfcefe461b

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 const init = () => {
+  // code related to DOM manipulation
+  const inputForm = document.querySelector('form');
   
+  inputForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const input = document.querySelector('input#searchByID');
+    
+    console.log(input.value); // console.log(e);
+  
+    fetch(`http://localhost:3000/movies/${input.value}`)
+    .then(response => response.json())
+    .then(data => {
+      // console.log(data);
+      // LOG: (3) [{...}, {...}, {...}]
+      const title = document.querySelector('section#movieDetails h4');
+      const summary = document.querySelector('section#movieDetails p');
+      
+      title.innerText = data.title;
+      summary.innerText = data.summary;
+    });
+  });
 }
 
 document.addEventListener('DOMContentLoaded', init);
